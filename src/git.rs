aed955807507755bf80bf8af5ccb90b1806523f2
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-30 15:49:15 +0300

    Write function to get changed files

--- src/git.rs
+++ src/git.rs
@@ -1,8 +1,8 @@
-use std::process::{Command};
+use std::process::Command;
 
-pub fn run_git(command: &str) -> Result<String, String> {
+pub fn run_git(commands: &[&str]) -> Result<String, String> {
   let output = Command::new("git")
-    .arg(command)
+    .args(commands)
     .output()
     .map_err(|e| e.to_string())?;
 
@@ -11,4 +11,9 @@ pub fn run_git(command: &str) -> Result<String, String> {
   } else {
     Err(String::from_utf8_lossy(&output.stderr).into())
   }
+}
+
+pub fn get_changed_files() -> Result<Vec<String>, String> {
+  let output = run_git(&["status", "--porcelain"]);
+  output.map(|s| s.lines().map(|line| line[3..].to_string()).collect())
 }
