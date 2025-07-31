9f90d93108c94e6f62e20e1e6b2db4b8ae3d4eb0
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-31 13:41:46 +0300

    Render main menu in accordance with new app.rs

--- src/interface.rs
+++ src/interface.rs
@@ -1,27 +1,55 @@
-use crate::{app::App, git::run_git};
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crate::app::App;
 use ratatui::{
-  DefaultTerminal, Frame,
-  buffer::Buffer,
-  layout::Rect,
-  style::Stylize,
-  symbols::border,
-  text::{Line, Text},
-  widgets::{Block, Borders, Paragraph, Widget},
+  prelude::*,
+  widgets::{Block, Borders, List, ListItem, Paragraph},
 };
-use std::io;
 
 pub trait Renderable {
-  fn render_main_menu(&self, f: &mut Frame);
-  fn render_commit_menu(&self, f: &mut Frame);
+  fn render_main_menu(&mut self, f: &mut Frame);
+  fn render_commit_menu(&mut self, f: &mut Frame);
 }
 
 impl Renderable for App {
-  fn render_main_menu(&self, f: &mut Frame) {
-    // codecodecodecodecode
+  fn render_main_menu(&mut self, f: &mut Frame) {
+    let main_layout = Layout::default()
+      .direction(Direction::Vertical)
+      .constraints([Constraint::Length(3), Constraint::Min(0)])
+      .split(f.area());
+
+    let branch_text = format!("branch: {}", self.current_branch);
+    let header = Paragraph::new(Text::from(branch_text).fg(Color::Green))
+      .block(Block::default().borders(Borders::BOTTOM))
+      .alignment(Alignment::Center);
+    f.render_widget(header, main_layout[0]);
+
+    let items: Vec<ListItem> = self
+      .changed_files
+      .iter()
+      .enumerate()
+      .map(|(i, file)| {
+        let prefix = if self.staged_indices.contains(&i) {
+          Span::styled("[x] ", Style::default().fg(Color::LightGreen))
+        } else {
+          Span::styled("[ ] ", Style::default().fg(Color::White))
+        };
+        let line = Line::from(vec![prefix, Span::raw(file.trim())]);
+        ListItem::new(line)
+      })
+      .collect();
+
+    let changes_list = List::new(items)
+      .block(Block::default().borders(Borders::ALL).title("Changes"))
+      .highlight_style(
+        Style::default()
+          .bg(Color::DarkGray)
+          .add_modifier(Modifier::BOLD),
+      )
+      .highlight_symbol(">> ");
+
+    f.render_stateful_widget(changes_list, main_layout[1], &mut self.main_menu_state);
   }
 
-  fn render_commit_menu(&self, f: &mut Frame) {
+  fn render_commit_menu(&mut self, f: &mut Frame) {
     // codecodecodecodecode
   }
 }
