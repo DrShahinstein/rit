706d055bc90b43bd9be0c3514d3809a1b30b1aeb
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-31 16:10:17 +0300

    Integrate keys.rs in App

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,13 @@
+use crate::keys::KeysInteractable;
 use crate::{git, interface::Renderable};
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyEvent, KeyEventKind};
 use ratatui::{Frame, prelude::*, widgets::ListState};
 use std::{collections::HashSet, io};
 
 type DefaultTerminal = Terminal<CrosstermBackend<io::Stdout>>;
 
 #[derive(Debug, Default)]
-enum RenderChoice {
+pub enum RenderChoice {
   #[default]
   MainMenu,
   CommitMenu,
@@ -14,7 +15,7 @@ enum RenderChoice {
 
 #[derive(Debug)]
 pub struct App {
-  render: RenderChoice,
+  pub render: RenderChoice,
   pub changed_files: Vec<String>,
   pub staged_indices: HashSet<usize>,
   pub commit_msg: String,
@@ -79,66 +80,12 @@ impl App {
 
   fn handle_key_event(&mut self, key_event: KeyEvent) {
     match self.render {
-      RenderChoice::MainMenu => self.handle_main_menu_keys(key_event),
-      RenderChoice::CommitMenu => { /* self.handle_commit_menu_keys(key_event) */ }
+      RenderChoice::MainMenu   => self.handle_main_menu_keys(key_event),
+      RenderChoice::CommitMenu => self.handle_commit_menu_keys(key_event),
     }
   }
 
-  fn handle_main_menu_keys(&mut self, key_event: KeyEvent) {
-    match key_event.code {
-      KeyCode::Char('q') => self.exit(),
-      KeyCode::Char('c') if !self.staged_indices.is_empty() => {
-        self.render = RenderChoice::CommitMenu;
-      }
-      KeyCode::Down | KeyCode::Char('j') => self.select_next(),
-      KeyCode::Up | KeyCode::Char('k') => self.select_previous(),
-      KeyCode::Enter => self.toggle_stage_selection(),
-      _ => {}
-    }
-  }
-
-  fn select_next(&mut self) {
-    if self.changed_files.is_empty() {
-      return;
-    }
-    let i = self.main_menu_state.selected().map_or(0, |i| {
-      if i >= self.changed_files.len() - 1 {
-        0
-      } else {
-        i + 1
-      }
-    });
-    self.main_menu_state.select(Some(i));
-  }
-
-  fn select_previous(&mut self) {
-    if self.changed_files.is_empty() {
-      return;
-    }
-    let i = self.main_menu_state.selected().map_or(0, |i| {
-      if i == 0 {
-        self.changed_files.len() - 1
-      } else {
-        i - 1
-      }
-    });
-    self.main_menu_state.select(Some(i));
-  }
-
-  fn toggle_stage_selection(&mut self) {
-    if let Some(selected_index) = self.main_menu_state.selected() {
-      let file_path = &self.changed_files[selected_index];
-      if self.staged_indices.contains(&selected_index) {
-        let _ = git::run_git(&["reset", "HEAD", "--", file_path]);
-        self.staged_indices.remove(&selected_index);
-      } else {
-        let _ = git::run_git(&["add", file_path]);
-        self.staged_indices.insert(selected_index);
-      }
-    }
-  }
-
-  fn exit(&mut self) {
+  pub fn exit(&mut self) {
     self.exit = true;
   }
 }
