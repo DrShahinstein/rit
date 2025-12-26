e38501919d06bb2b76d24d03ebc0608db32a189e
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-26 17:58:47 +0300

    Improve key management based on render choice (keys.rs)

--- src/tui/keys.rs
+++ src/tui/keys.rs
@@ -1,9 +1,28 @@
 use crossterm::event::KeyCode;
-use super::app::App;
+use super::app::{App, RenderChoice};
 
 pub fn handle_keys(app: &mut App, key: KeyCode) {
   match key {
-    KeyCode::Char('q') | KeyCode::Esc => app.shutdown(),
+    KeyCode::Char('q') => app.shutdown(),
+    _ => {}
+  }
+
+  match app.get_render_choice() {
+    RenderChoice::MainMenu   => main_menu_keys(app, key),
+    RenderChoice::CommitMenu => commit_menu_keys(app, key),
+  }
+}
+
+fn main_menu_keys(app: &mut App, key: KeyCode) {
+  match key {
+    KeyCode::Char('c') => app.go_commit(),
+    _ => {}
+  }
+}
+
+fn commit_menu_keys(app: &mut App, key: KeyCode) {
+  match key {
+    KeyCode::Esc => app.go_main(),
     _ => {}
   }
 }
