73a19358540b722befc4ab2413411f3e40413a98
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-26 17:26:03 +0300

    Call render_choice's getter to know about it (ui.rs)

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,12 +1,12 @@
+#[allow(unused_imports)]
 use ratatui::{Frame, widgets::{*}, layout::{*}};
 use super::app::{App, RenderChoice};
 
 pub fn draw(app: &App, frame: &mut Frame) {
-  match app.render_choice {
+  match app.get_render_choice() {
     RenderChoice::MainMenu => {
       frame.render_widget("Main", frame.area());
     }
-
     RenderChoice::CommitMenu => {
       frame.render_widget("Commit", frame.area());
     }
