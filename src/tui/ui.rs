e1807f39449886ee8ee3c9670cbdff3e816f1a93
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-27 16:07:01 +0300

    Render what branch it is (ui.rs)

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,14 +1,28 @@
 #[allow(unused_imports)]
-use ratatui::{Frame, widgets::{*}, layout::{*}};
+use ratatui::{prelude::{*}, widgets::{*}};
 use super::app::{App, RenderChoice};
 
 pub fn draw(app: &App, frame: &mut Frame) {
+  let chunks =  Layout::default()
+    .direction(Direction::Vertical)
+    .constraints([Constraint::Length(1), Constraint::Min(0)])
+    .split(frame.area());
+
+  let branch = app.get_branch();
+  let style  = Style::default().fg(Color::Green);
+
+  let header = Paragraph::new(branch)
+    .alignment(Alignment::Center)
+    .style(style);
+
+  frame.render_widget(header, chunks[0]);
+
   match app.get_render_choice() {
     RenderChoice::MainMenu => {
-      frame.render_widget("Main", frame.area());
+      frame.render_widget("Main", chunks[1]);
     }
     RenderChoice::CommitMenu => {
-      frame.render_widget("Commit", frame.area());
+      frame.render_widget("Commit", chunks[1]);
     }
   }
 }
