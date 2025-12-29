41efa361ff6ff168db1f44db41cd09adb4cd234e
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-29 14:04:36 +0300

    Create 'git::file' submodule and refine return types of git funcs (git.rs)

--- src/tui/git.rs
+++ src/tui/git.rs
@@ -1,23 +1,42 @@
 use std::process::Command;
+use color_eyre::{Result, eyre::bail};
 
-pub fn run(commands: &[&str]) -> Result<String, String> {
-  let output = Command::new("git")
-    .args(commands)
-    .output()
-    .map_err(|e| e.to_string())?;
+#[allow(dead_code)]
+pub mod file {
+  #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+  pub enum Status {
+    Modified,  // 'M'
+    Added,     // 'A'
+    Deleted,   // 'D'
+    Renamed,   // 'R'
+    Copied,    // 'C'
+    Unmerged,  // 'U'
+    Untracked, // '?'
+  }
+
+  #[derive(Debug, Clone)]
+  pub struct Changed {
+    pub path:   String,
+    pub status: Status,
+  }
+}
+
+pub fn run(commands: &[&str]) -> Result<String> {
+  let output = Command::new("git").args(commands).output()?;
 
   if output.status.success() {
-    String::from_utf8(output.stdout).map_err(|e| e.to_string())
-  }
-  else {
-    Err(String::from_utf8_lossy(&output.stderr).into())
+    Ok(String::from_utf8(output.stdout)?)
+  } else {
+    bail!("{}", String::from_utf8_lossy(&output.stderr))
   }
 }
 
-pub fn get_branch() -> Option<String> {
+pub fn get_branch() -> Result<String> {
   run(&["branch", "--show-current"])
-    .ok()
     .map(|s| s.trim().to_string())
-    .filter(|s| !s.is_empty())
 }
 
+pub fn _get_changed_files() -> Result<Vec<file::Changed>> {
+  let _gitstatus = run(&["status", "--porcelain=v1"])?;
+  Ok(Vec::new())
+}
