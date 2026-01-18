d453b52e65a7fc47fc6ec8e29b52c96492c341e9
DrShahinstein <1taylanozturk@protonmail.com>
2026-01-18 18:10:04 +0300

    Adjust enter key to handle selected action (keys.rs)

--- src/tui/keys.rs
+++ src/tui/keys.rs
@@ -1,5 +1,5 @@
 use ratatui::crossterm::event::{KeyEvent, KeyCode, KeyModifiers};
-use super::app::{App, RenderChoice};
+use super::app::{App, RenderChoice, SelectionMode};
 
 pub fn handle_keys(app: &mut App, key: KeyEvent) {
   match app.get_render_choice() {
@@ -10,12 +10,13 @@ pub fn handle_keys(app: &mut App, key: KeyEvent) {
 
 fn main_menu_keys(app: &mut App, key: KeyEvent) {
   match key.code {
-    KeyCode::Char('q') => { app.shutdown(); return; },
-    KeyCode::Char('c') =>   app.go_commit(),
-    KeyCode::Char('r') =>   app.refresh(),
-    KeyCode::Down      =>   app.select_next(),
-    KeyCode::Up        =>   app.select_prev(),
-    KeyCode::Enter     =>   app.toggle_file_stage(),
+    KeyCode::Char('q')             => { app.shutdown(); return; },
+    KeyCode::Char('c')             => app.go_commit(),
+    KeyCode::Char('r')             => app.refresh(),
+    KeyCode::Down                  => app.select_next(),
+    KeyCode::Up                    => app.select_prev(),
+    KeyCode::Left | KeyCode::Right => app.toggle_selection_mode(),
+    KeyCode::Enter                 => little_help::do_enter(app),
     _ => {}
   }
 }
@@ -33,4 +34,15 @@ fn commit_menu_keys(app: &mut App, key: KeyEvent) {
   }
 
   app.get_textarea_mut().input(key);
+}
+
+mod little_help {
+  use super::{App, SelectionMode};
+
+  pub fn do_enter(app: &mut App) {
+    match app.get_selection_mode() {
+      SelectionMode::Stage   => app.toggle_file_stage(),
+      SelectionMode::Discard => app.discard_file(),
+    }
+  }
 }
